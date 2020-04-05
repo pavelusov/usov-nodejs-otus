c7c39e276b6a115eec5b9b722b08c1a6afd1eb1f
pavel.usov <pavel.usov@motorsport.com>
2020-04-05 20:09:50 +0500

    Added a showTree utility, a formatter for console.info

---
+++ src/tree-func-01/data.ts
@@ -0,0 +1,35 @@
+export const data = {
+  "name": 1,
+  "items": [
+    {
+      "name": 2,
+      "items": [
+        {
+          "name": 3
+        },
+        {
+          "name": 4,
+          "items": [
+            {
+              "name": 7
+            },
+            {
+              "name": 8,
+              "items": [
+                {
+                  "name": 9
+                },
+                {
+                  "name": 10
+                },
+                {
+                  "name": 11
+                },
+              ]
+            }]
+        }]
+    }, {
+      "name": 5,
+      "items": [{ "name": 6 }]
+    }]
+};
