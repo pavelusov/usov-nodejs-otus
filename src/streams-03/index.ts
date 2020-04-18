077aa378449b138dcbc8367079959044f23a7ed9
pavel.usov <pavel.usov@motorsport.com>
2020-04-18 19:29:32 +0500

    Added sort merge for streams

--- src/streams-03/index.ts
+++ src/streams-03/index.ts
@@ -4,11 +4,16 @@ import fs from 'fs';
 import {
   getMaxSizeMemory,
   fileGenerate,
-  sortMerge
+  sortMerge,
 } from './helpers';
 
 const filePath = path.join(__dirname, '.', 'numbers');
 
+interface ITempData {
+  data: string;
+  size: number;
+  partFile: number;
+}
 
 export default function main(argv: string[]) {
   const MEMORY_BUFFER_DIVIDER = 2;
@@ -16,74 +21,136 @@ export default function main(argv: string[]) {
   fileGenerate(filePath, 1); // MB
 
   const reader = fs.createReadStream(filePath, { encoding: 'utf8' });
-  let partFile = 1;
-  let data: string;
-  let size = 0; // KB
+  const temp: ITempData = {
+    data: '',
+    size: 0,
+    partFile: 1,
+  };
 
   reader.on('data', (chunk) => {
     reader.pause();
+    temp.data += chunk;
+    temp.size += chunk.length;
 
-    data += chunk;
-    size += chunk.length;
-
-    if (size <= maxSizeMemory / MEMORY_BUFFER_DIVIDER) {
+    if (temp.size <= maxSizeMemory / MEMORY_BUFFER_DIVIDER) {
       reader.resume();
     } else {
-      // SORT START
-      data = data.replace(/undefined/g, '');
-
-      let sortedNumberList = data
-        .split(' ')
-        .map(stringNumber => Number(stringNumber));
-
-      sortedNumberList = sortMerge(sortedNumberList);
-      // SORT END
-
-      // WRITE SORTED FILE START
-      writeDataSync('./sortedArrays', 'part', partFile++, sortedNumberList);
-      // WRITE SORTED FILE END
-
-      // Next file parts
-      data = '';
-      size = 0;
-      reader.resume();
+      writePart(temp, reader);
     }
   });
 
   reader.on('end', () => {
+    writePart(temp, reader);
+  });
+
+  reader.on('close', () => {
     const sortedFileDirPath = path.join(__dirname, '.', 'sortedArrays');
-    let sortedFileNames = fs.readdirSync(sortedFileDirPath);
-    // Remove hidden files
-    sortedFileNames = sortedFileNames.filter(name => !(/^\./.test(name)));
-    let dataTemp: {[key:number]: number[]} = {};
-    let data: string;
-    sortedFileNames.forEach((nameFile, i) => {
 
-      const sortedFilePath = path.join(sortedFileDirPath, nameFile);
-      const reader = fs.createReadStream(sortedFilePath, { encoding: 'utf8' });
-      reader.on('data', (chunk) => {
+    fileMerge(sortedFileDirPath);
+  });
+}
+
+function fileMerge(pathDir: string) {
+  const SIDES = 2;
+  let names = fs.readdirSync(pathDir);
+  const lastDirName = names[names.length - 1];
+  let matchedIndex = lastDirName.match(/\d+$/);
+  let lastFileIndex = 0;
+
+  if (matchedIndex) lastFileIndex = Number(matchedIndex[0]);
+
+  const writer = fs.createWriteStream(path.join(pathDir, `result${++lastFileIndex}`));
+
+  // Remove hidden files
+  names = names.filter(name => !(/^\./.test(name)));
+  if (names.length > SIDES) {
+    const leftFileName = path.join(pathDir, names[0]);
+    const rightFileName = path.join(pathDir, names[1]);
+    let left: number[];
+    let right: number[];
+
+
+    for (let i = 0; i < SIDES; i++) {
+      const fileName = path.join(pathDir, names[i]);
+      const reader = fs.createReadStream(fileName, { encoding: 'utf8'});
+
+      reader.on('data', (chunk => {
         reader.pause();
-        dataTemp[Number(i)] = chunk
-          .toString()
-          .split(',')
-          .map(strNumber => Number(strNumber));
 
-        // Last reader
-        if (i === sortedFileNames.length - 1) {
-          // for (let j = 0; j < dataTemp[0].length; j++) {
-          for (let j = 0; j < 10; j++) {
-            console.log('************************')
-            for (let t = 0; t <= i; t++) {
-              console.log('file: part', t + 1, '===', dataTemp[t][j]);
+        if (i === 0) {
+          left = chunk
+            .toString()
+            .split(',')
+            .map(stringNumber => Number(stringNumber));
+        } else {
+          right = chunk
+            .toString()
+            .split(',')
+            .map(stringNumber => Number(stringNumber));
+
+          const leftLength = typeof left.length === 'undefined' ? 0 : left.length;
+          const rightLength = typeof right.length === 'undefined' ? 0 : right.length;
+          let l = 0;
+          let r = 0;
+
+          while (l < leftLength && r < rightLength) {
+            const rightValue = right[r];
+            const leftValue = left[l];
+            if (rightValue > leftValue) {
+              writer.write(writeValue(leftValue));
+              l++;
+            } else {
+              writer.write(writeValue(rightValue));
+              r++;
             }
           }
 
+          while (l < leftLength) {
+            const leftValue = left[l];
+            writer.write(writeValue(leftValue));
+            l++;
+          }
+
+          while (r < rightLength) {
+            const rightValue = right[r];
+            writer.write(writeValue(rightValue));
+            r++;
+          }
+
+          // Remove those files
+          fs.unlinkSync(leftFileName);
+          fs.unlinkSync(rightFileName);
+          writer.close();
+          fileMerge(pathDir);
         }
-      })
-    })
+      }))
+    }
+  }
+}
 
-  });
+function writeValue(value: number): string {
+  return `${value},`;
+}
 
+function writePart(options: ITempData, reader: fs.ReadStream) {
+  let { data } = options;
+// SORT START
+  data = data.replace(/undefined/g, '');
+  let sortedNumberList = data
+    .split(' ')
+    .map(stringNumber => Number(stringNumber));
+
+  sortedNumberList = sortMerge(sortedNumberList);
+  // SORT END
+
+  // WRITE SORTED FILE START
+  writeDataSync('./sortedArrays', 'part', options.partFile++, sortedNumberList);
+  // WRITE SORTED FILE END
+
+  // Next file parts
+  options.data = '';
+  options.size = 0;
+  reader.resume();
 }
 
 function createWriter(dirName: string, fileName: string, partFile: number): fs.WriteStream {
